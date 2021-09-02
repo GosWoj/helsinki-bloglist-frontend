ba29020b3c90886f38b97ec2e479ea7fe79aeaaa
GosWoj <gosiawoojcik@gmail.com>
2021-09-02 14:14:47 +0200

    step1 done - added login form and function, conditional rendering for users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import * as loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [name, setName] = useState("");
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      setUser(user);
+      setName(user.name);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {!user ? (
+        <div>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+            <div>
+              Username:
+              <input
+                value={username}
+                name="username"
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              Password:
+              <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <h2>blogs</h2>
+          <h3>{name} logged in</h3>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
