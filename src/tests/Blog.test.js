68abac2e9b0686bf6977d007d81ecd775b34ebac
GosWoj <gosiawoojcik@gmail.com>
2021-09-08 15:27:25 +0200

    added beforeEach to make tests and code cleaner

--- src/tests/Blog.test.js
+++ src/tests/Blog.test.js
@@ -3,22 +3,9 @@ import "@testing-library/jest-dom/extend-expect";
 import { render, fireEvent } from "@testing-library/react";
 import Blog from "../components/Blog";
 
-test("<Blog /> displays only title when rendered", () => {
-  const blog = {
-    author: "Test Testing",
-    title: "Testing with react",
-    url: "www.google.com",
-    likes: 99,
-  };
+let component;
 
-  const component = render(<Blog blog={blog} />);
-
-  expect(component.container).toHaveTextContent("Testing with react");
-  expect(component.container).not.toHaveTextContent("www.google.com");
-  expect(component.container).not.toHaveTextContent(99);
-});
-
-test("<Blog /> displays details when button is clicked", () => {
+beforeEach(() => {
   const blog = {
     author: "Test Testing",
     title: "Testing with react",
@@ -30,11 +17,19 @@ test("<Blog /> displays details when button is clicked", () => {
   };
 
   const user = {
-    username: "test1",
+    username: "test",
   };
 
-  const component = render(<Blog blog={blog} user={user} />);
+  component = render(<Blog blog={blog} user={user} />);
+});
 
+test("<Blog /> displays only title when rendered", () => {
+  expect(component.container).toHaveTextContent("Testing with react");
+  expect(component.container).not.toHaveTextContent("www.google.com");
+  expect(component.container).not.toHaveTextContent(99);
+});
+
+test("<Blog /> displays details when button is clicked", () => {
   const button = component.getByText("View");
   fireEvent.click(button);
 
