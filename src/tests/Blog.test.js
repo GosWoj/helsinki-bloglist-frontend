02d83df1b5d7bcb718fe3ac8f5cd4cf13d019656
GosWoj <gosiawoojcik@gmail.com>
2021-09-08 15:20:56 +0200

    tests step2 done - checks if additional data is displayed when button clicked

--- src/tests/Blog.test.js
+++ src/tests/Blog.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Blog from "../components/Blog";
 
 test("<Blog /> displays only title when rendered", () => {
@@ -16,4 +16,28 @@ test("<Blog /> displays only title when rendered", () => {
   expect(component.container).toHaveTextContent("Testing with react");
   expect(component.container).not.toHaveTextContent("www.google.com");
   expect(component.container).not.toHaveTextContent(99);
+});
+
+test("<Blog /> displays details when button is clicked", () => {
+  const blog = {
+    author: "Test Testing",
+    title: "Testing with react",
+    url: "www.google.com",
+    likes: 99,
+    user: {
+      username: "test",
+    },
+  };
+
+  const user = {
+    username: "test1",
+  };
+
+  const component = render(<Blog blog={blog} user={user} />);
+
+  const button = component.getByText("View");
+  fireEvent.click(button);
+
+  expect(component.container).toHaveTextContent("Test Testing");
+  expect(component.container).toHaveTextContent(99);
 });
